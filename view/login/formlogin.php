e24f689e4269c73ffdbccb9c8408639cef0960f2
Huỳnh huy hoàng <45021513+hhhdn9999@users.noreply.github.com>
2020-07-22 17:56:00 +0700

    Update formlogin.php

--- view/login/formlogin.php
+++ view/login/formlogin.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="#" method="post">
+    <form action="/controller/index.php" method="post">
         <div class="login">
             <div class="login-screen">
                 <div class="app-title">
